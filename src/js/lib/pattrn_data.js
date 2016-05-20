33a1d3e386a172235d84ae55574f4fe81bff15d4
andrea rota <a@xelera.eu>
2016-05-20 15:14:56 +0100

    move common functions to own module

---
+++ src/js/lib/pattrn_data.js
@@ -0,0 +1,35 @@
+/*
+Copyright (C) 2016 andrea rota <a@xelera.eu>
+The array_equals() function is Copyright (C) 2013 Tomáš Zato,
+distributed under CC-BY-SA-3.0 and published here:
+http://stackoverflow.com/a/14853974/550077
+
+This file is part of Pattrn - http://pattrn.co/.
+
+Pattrn is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Pattrn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Pattrn.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { is_defined } from './utils/is_defined.js';
+
+export function count_rows_with_data(dataset, item) {
+  return dataset.reduce(function(pv, cv, ci, a) {
+    return (is_defined(cv[item]) && cv[item] !== "") ? pv + 1 : pv;
+  }, 0);
+}
+
+export function replace_undefined_values (parameters, variable) {
+  if (!is_defined(parameters.row[variable]) || parameters.row[variable] === "") {
+    parameters.dataset[parameters.index][variable] = parameters.empty_value;
+  }
+}

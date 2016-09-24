5055f6ba81ef6d32e4a3545e18e59c0b2dd6c341
andrea rota <a@xelera.eu>
2016-09-24 18:25:56 +0100

    refactor dropping of rows with invalid datetimes
    previously we were using filter().map(), now we use
    map().filter() to make sure all cases are covered, and we log
    to console any rows we drop and the reason for dropping them

--- src/js/lib/data/add_dateobject_to_rows.js
+++ src/js/lib/data/add_dateobject_to_rows.js
@@ -17,7 +17,6 @@ You should have received a copy of the GNU Affero General Public License
 along with Pattrn.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import d3 from 'd3';
 import { log_dropped_row } from '../utils/hello_devs.js';
 import { is_defined } from '../utils/is_defined.js';
 
@@ -34,24 +33,42 @@ export default function add_dateobject_to_rows(dataset) {
    * lack of consistency across Pattrn instances (but do we care? just pick
    * a sensible default (ISO8601?) and document things accurately)
    */
-  let dateFormat = d3.time.format('%Y-%m-%dT%H:%M:%S');
+  let date_format_string = '%Y-%m-%dT%H:%M:%S';
+  let dateFormat = d3.time.format(date_format_string);
 
   // Remove rows with invalid dates
   dataset = dataset
-    .filter(function(d) {
-      try {
-        dateFormat.parse(d.date_time);
-      } catch (e) {
-        return false;
-      }
-      return true;
-    })
     .map(function(d) {
-      // Add dd member to each case, holding a date object
-      // (this is what is used throughout Pattrn, since Pattrn v1.0)
-      d.dd = dateFormat.parse(d.date_time);
+      let date_object;
+
+      /**
+       * Parse datetime, set to null if we trip an exception or if datetime
+       * cannot be parsed; rows with null d.dd will then be filtered out
+       */
+      try {
+        date_object = dateFormat.parse(d.date_time);
+      } catch (e) {
+        date_object = null;
+      }
+
+      /**
+       * Add dd member to each case, holding a date object
+       * (this is what is used throughout Pattrn, since Pattrn v1.0).
+       * d.dd will be null if we tripped an exception while parsing d.date_time
+       * or of d3.date.format().parse() returned null because of not being
+       * able to parse d.date_time.
+       */
+      d.dd = date_object;
 
       return d;
+    })
+    .filter(function(d) {
+      if(is_defined(d.dd)) {
+        return true;
+      } else {
+        log_dropped_row(d, `The date format used for this row's date_time (${d.date_time}) does not match the d3.time.format configured for this instance (${date_format_string})`);
+        return false;
+      }
     });
 
   return dataset;
